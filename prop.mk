a8e87b97428d8004ce4091f8a16cc491ccc572eb
Dan Stoza <stoza@google.com>
2016-07-09 09:55:05 +0000

    rosy: Set DispSync offsets of -4/-8 ms for SF/app
    Moves SurfaceFlinger to 4 ms before vsync to optimize latency in light of the latch-on-acquire-fence-signal change, and moves the app to 4 ms before SurfaceFlinger to give it up to 20 ms for the acquire fence generated by its rendering to fire.
    
    Bug: 29413700
    
    Change-Id: I6ab1b502de05a2823dfb29d7dc40fb8be1252cbd

--- prop.mk
+++ prop.mk
@@ -191,6 +191,8 @@ persist.sys.fflag.override.settings_network_and_internet_v2=true
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
 ro.surface_flinger.max_virtual_display_dimension=4096 \
+ro.surface_flinger.vsync_event_phase_offset_ns=4000000 \
+ro.surface_flinger.vsync_sf_event_phase_offset_ns=8000000 \
 ro.surface_flinger.protected_contents=true \
 debug.sf.enable_gl_backpressure=1
 
