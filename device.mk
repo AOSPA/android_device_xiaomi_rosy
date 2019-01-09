0188da60bc3a5f2ac305c4bc9c3c21a3498d4ce3
shoudil <stephenli@codeaurora.org>
2019-01-09 17:04:11 +0800

    rosy: Add vendor variant of framework detect modules
    Add JNI and libqti_vndfwk_detect module name to product_packages
    to specify module to be installed to device. Here explicitly
    specify both the core and vendor variant in PRODUCT_PACKAGES.
    
    CRs-Fixed: 2372775
    
    Change-Id: Ic64f5ac0cfbbe9e105a6b48b5bc7687c8d23d7e8
    Signed-off-by: Vishalcj17 <Vishalcj17@gmail.com>

--- device.mk
+++ device.mk
@@ -330,7 +330,9 @@ PRODUCT_COPY_FILES += \
 
 # QMI
 PRODUCT_PACKAGES += \
-    libjson
+    libjson \
+    libqti_vndfwk_detect \
+    libqti_vndfwk_detect.vendor
 
 # QTI Performance
 PRODUCT_COPY_FILES += \
