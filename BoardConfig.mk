c24e6b2a85edc665d89e912b23521c7976ef459a
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-01-14 21:09:06 +0530

    Correct define libinit variable
    Fixes:
    
    15:55:35 error: system/core/init/Android.bp:95:1: "libinit" depends on undefined module "//:libinit_rosy"
    
    Change-Id: Ia1a58bea3df625fd76c060370dc7f9fa2a597765
    Signed-off-by: Melvin <kartik3163@gmail.com>
    Signed-off-by: Vishalcj17 <vishalcj17@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -151,7 +151,7 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
 
 # Init
-TARGET_INIT_VENDOR_LIB := libinit_rosy
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_rosy
 TARGET_RECOVERY_DEVICE_MODULES := libinit_rosy
 
 # Keymaster
