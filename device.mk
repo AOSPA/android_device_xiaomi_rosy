34e93d0ccc6389f9af715d11aa8e0cd98e92f496
Vishalcj17 <vishalcj17@gmail.com>
2020-02-08 10:26:42 +0530

    rosy: remove -qti suffix from power *Don't build mock too
    Signed-off-by: Vishalcj17 <vishalcj17@gmail.com>

--- device.mk
+++ device.mk
@@ -320,8 +320,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power@1.2-service \
+    android.hardware.power@1.2-impl
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
