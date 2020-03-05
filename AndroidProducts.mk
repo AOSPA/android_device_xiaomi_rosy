melvinstark3/device_asus_X00T

Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
Signed-off-by: Melvin <kartik3163@gmail.com>

AndroidProducts.mk
@@ -16,3 +16,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/syberia_X00T.mk
+
+COMMON_LUNCH_CHOICES := \
+    syberia_X00TD-userdebug \
+    syberia_X00TD-user \
+    syberia_X00TD-eng

AOSPA/android_device_xiaomi_rosy

AndroidProducts.mk
@@ -15,3 +15,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_rosy.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineeage_rosy-userdebug \
+    lineage_rosy-user \
+    lineage_rosy-eng