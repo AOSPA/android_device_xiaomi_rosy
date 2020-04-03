797739a72aeb7ad8f879d33dc29c501f85e7ea4a
Vishalcj17 <Vishalcj17@gmail.com>
2020-04-03 08:57:00 +0530

    rosy: Cleanup tree
    Signed-off-by: Vishalcj17 <Vishalcj17@gmail.com>
    Change-Id: Iaccd030783a2fbc7149e2d24b062c4b2a1bbc9db

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -192,11 +192,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
 # Recovery
-ifeq ($(AB_OTA_UPDATER), true)
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/fstab_AB.qcom
-else
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/fstab.qcom
-endif
 
 # Security Patch Level
 VENDOR_SECURITY_PATCH := 2019-05-01
