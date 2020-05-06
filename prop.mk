0713ab9ea6e3a4861f8c3aa095b73b7b2a81212a
Vishalcj17 <vishalcj@aospa.co>
2020-05-06 15:26:42 +0200

    rosy: Cleanup Display props and flags
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>
    Change-Id: I1b125d6184001105823a4f484375df78f20746ad

--- prop.mk
+++ prop.mk
@@ -92,22 +92,17 @@ debug.sf.enable_hwc_vds=1 \
 debug.sf.hw=0 \
 debug.sf.latch_unsignaled=1 \
 debug.sf.recomputecrop=0 \
-debug.sf.enable_gl_backpressure=1 \
-debug.sdm.support_writeback=0 \
 dev.pm.dyn_samplingrate=1 \
 persist.demo.hdmirotationlock=false \
 persist.hwc.enable_vds=1 \
 persist.hwc.mdpcomp.enable=true \
 ro.opengles.version=196610 \
 ro.qualcomm.cabl=0 \
-ro.sf.lcd_density=320 \
 ro.vendor.display.cabl=2 \
 sdm.debug.disable_skip_validate=1 \
 vendor.display.disable_skip_validate=1 \
 vendor.display.enable_default_color_mode=1 \
 vendor.gralloc.enable_fb_ubwc=1 \
-sys.display-size=1920x1080 \
-debug.sf.hwc.canUseABC=1 \
 debug.sf.disable_backpressure=1
 
 # DRM
