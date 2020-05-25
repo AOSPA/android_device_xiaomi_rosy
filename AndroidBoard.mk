4bf3c9378215298a99e821fc933e41df60d54590
Vishalcj17 <vishalcj@aospa.co>
2020-05-25 16:21:32 +0200

    rosy: let's ship firmware
    MIUI 11 Oreo 8.1 Global firmware
    Also, add aospa splash.img
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>
    Change-Id: Ic8115647ad3268cbb9532d37cf5cd9e553ba7c15

---
+++ AndroidBoard.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+ifeq ($(ADD_RADIO_FILES), true)
+radio_dir := $(LOCAL_PATH)/radio
+RADIO_FILES := $(shell cd $(radio_dir) ; ls)
+$(foreach f, $(RADIO_FILES), \
+    $(call add-radio-file,radio/$(f)))
+endif
