c643cf8f598a09d8f716ee4bb7b5232d444682f4
Vishalcj17 <vishalcj@aospa.co>
2020-06-08 21:27:40 +0200

    rosy: remove custom VSYNC offsets and triple frame buffer.
    this, on rosy, cause performance regression and also introduces unwanted jank,
    so, removing these props.
    
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>
    Change-Id: I4898826e673805279351f2dfb9ce180054247ece

--- prop.mk
+++ prop.mk
@@ -225,19 +225,10 @@ persist.sys.fflag.override.settings_network_and_internet_v2=true
 
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
-ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000 \
 ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
-ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
 ro.surface_flinger.max_virtual_display_dimension=4096 \
-ro.surface_flinger.protected_contents=true
-
-PRODUCT_PROPERTY_OVERRIDES += \
-debug.sf.enable_gl_backpressure=1 \
-debug.sf.early_phase_offset_ns=1500000 \
-debug.sf.early_app_phase_offset_ns=1500000 \
-debug.sf.early_gl_phase_offset_ns=3000000 \
-debug.sf.early_gl_app_phase_offset_ns=15000000
+ro.surface_flinger.protected_contents=true \
+debug.sf.enable_gl_backpressure=1
 
 # Paranoid Doze
 PRODUCT_PROPERTY_OVERRIDES += \
