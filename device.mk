39a1a9ce3c0149d7eec486dda4d1d49df3507bd6
Vishalcj17 <vishalcj@aospa.co>
2020-09-29 10:36:18 +0530

    rosy: remove init.qcom.usb.rc
    already defined in platform.
    
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>
    Change-Id: I00c1e5fb25694a89710c25ef1eccd618499ab160

--- device.mk
+++ device.mk
@@ -271,7 +271,6 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     init.qcom.rc \
     init.qcom.sh \
-    init.qcom.usb.rc \
     init.recovery.qcom.rc \
     init.recovery.qcom.usb.rc \
     init.rosy.rc \
