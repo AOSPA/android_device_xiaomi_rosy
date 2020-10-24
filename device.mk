ef37f6092fcbb6c63277932bbffa01e6a1e9cbca
Vishalcj17 <vishalcj@aospa.co>
2020-10-24 17:00:36 +0530

    rosy: build CapabilityConfigStore from source
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>
    Change-Id: I0bd62a1c6d9b9e80e11d218f8b0ca2bcbff003c7

--- device.mk
+++ device.mk
@@ -247,7 +247,9 @@ PRODUCT_PACKAGES += \
     libOmxSwVencHevc \
     libOmxVdec \
     libOmxVenc \
-    libstagefrighthw
+    libstagefrighthw \
+    vendor.qti.hardware.capabilityconfigstore@1.0 \
+    vendor.qti.hardware.capabilityconfigstore@1.0.vendor
 
 # ParanoidDoze
 PRODUCT_PACKAGES += \
