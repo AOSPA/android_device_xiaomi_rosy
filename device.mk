2372ffb7e7cf2e5cdd99aaf9ac5b9214a742e187
Vishalcj17 <vishalcj@aospa.co>
2020-10-27 14:27:42 +0530

    rosy: audio: dont copy anything else except mixer_path
    ACDB IDs have been changed accroding to caf so that it works by default and i dont have to maintain anything audio now so yay.
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>
    Change-Id: Iccfec5119b44515f9b9aaf32db0e1a569d00b40a

--- device.mk
+++ device.mk
@@ -85,9 +85,7 @@ PRODUCT_PACKAGES += \
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_policy_configuration.xml \
-    $(LOCAL_PATH)/configs/audio/mixer_paths_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_mtp.xml \
-    $(LOCAL_PATH)/configs/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml
+    $(LOCAL_PATH)/configs/audio/mixer_paths_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_mtp.xml
 
 # Bluetooth
 PRODUCT_PACKAGES += \
